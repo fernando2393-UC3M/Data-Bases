9f3d73f17ceb3d09746cb39728032ac9c701c8fb
fernando2393 <garciasanzfernando2393@gmail.com>
2017-04-15 14:48:53 +0200

    Comments to explain the problem

--- Query4.sql
+++ Query4.sql
@@ -10,3 +10,10 @@ ON (CASTS.TITLE=TAPS_MOVIES.TITLE)
 )
 WHERE (YEAR=MOVIES.TITLE_YEAR)
 );
+
+
+--We need first to group actors by the number of taps each month and select the one who has the greatest number --> ROWNUM <=1
+--To do it, we need to use the date which is an attribute of the TAPS_MOVIES, but only year and month are relevant to order them
+--We need a table which join the attributes actor and title from cast with taps movies, whose relevant attributes are title, to compare with
+--the one obtained from cast, and the date, to later group by actor's name
+--Trouble: select the correct fuctions of sql server environment
