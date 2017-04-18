980462950b583ccf598873805770239a7574c3ee
fernando2393 <garciasanzfernando2393@gmail.com>
2017-04-18 21:26:13 +0200

    Query4

--- Query4.sql
+++ Query4.sql
@@ -1,14 +1,27 @@
-SELECT TO_CHAR(VIEW_DATETIME, 'MON'), MAX(COUNTER) FROM
-(SELECT ACTOR, TO_CHAR(VIEW_DATETIME, 'MON'), COUNT (TITLE) COUNTER FROM
+SELECT ACTOR, A.MON, MAXIMUM FROM(
+(SELECT MON, MAX(COUNTER) AS MAXIMUM FROM
+(SELECT ACTOR, TO_CHAR(VIEW_DATETIME, 'MON-YYYY') AS MON, COUNT (TITLE) COUNTER FROM
 (SELECT TITLE, VIEW_DATETIME, ACTOR
 FROM
 TAPS_MOVIES NATURAL JOIN
 CASTS)
-GROUP BY ACTOR, TO_CHAR(VIEW_DATETIME, 'MON')
+GROUP BY ACTOR, TO_CHAR(VIEW_DATETIME, 'MON-YYYY')
 ORDER BY COUNTER DESC)
-GROUP BY TO_CHAR(VIEW_DATETIME, 'MON');
+GROUP BY MON
+ORDER BY MON DESC) A
+JOIN
+(SELECT ACTOR, TO_CHAR(VIEW_DATETIME, 'MON-YYYY') AS MON, COUNT (TITLE) COUNTER FROM
+(SELECT TITLE, VIEW_DATETIME, ACTOR
+FROM
+TAPS_MOVIES NATURAL JOIN
+CASTS)
+GROUP BY ACTOR, TO_CHAR(VIEW_DATETIME, 'MON-YYYY')
+ORDER BY COUNTER DESC) B
+ON (MAXIMUM=COUNTER AND A.MON=B.MON)
+) ORDER BY TO_DATE(A.MON, 'MON-YYYY')
+;
 
---We need first to group actors by the number of taps each month and select the one who has the greatest number --> ROWNUM <=1
+--We need first to group actors by the number of taps each month and select the one who has the greatest number
 --To do it, we need to use the date which is an attribute of the TAPS_MOVIES, but only year and month are relevant to order them
 --We need a table which join the attributes actor and title from cast with taps movies, whose relevant attributes are title, to compare with
 --the one obtained from cast, and the date, to later group by actor's name
