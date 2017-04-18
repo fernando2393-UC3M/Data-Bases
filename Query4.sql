b11b6e57ada3b8afd827555d15026477573933e4
fernando2393 <garciasanzfernando2393@gmail.com>
2017-04-18 17:33:32 +0200

    New changes

--- Query4.sql
+++ Query4.sql
@@ -1,12 +1,15 @@
 SELECT ACTOR FROM
 (SELECT ACTOR, COUNT('x') VIEW_DATETIME FROM
-(SELECT TITLE AS TAPS_TITLE, VIEW_DATETIME, ACTOR
+(SELECT TITLE, VIEW_DATETIME, ACTOR
 FROM TAPS_MOVIES NATURAL JOIN
 CASTS
 )
 GROUP BY ACTOR
 ORDER BY VIEW_DATETIME DESC)
 WHERE ROWNUM <=1;
+
+
+SELECT TO_CHAR(VIEW_DATETIME, 'YYYY-MM') FROM
 
 --We need first to group actors by the number of taps each month and select the one who has the greatest number --> ROWNUM <=1
 --To do it, we need to use the date which is an attribute of the TAPS_MOVIES, but only year and month are relevant to order them
